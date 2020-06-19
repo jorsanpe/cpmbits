f9ce91f553e4b5a3541e3ff274e3122eb68b878e
Jordi Sánchez <jordi.sanchez@casa-systems.com>
2020-06-19 09:24:23 +0200

    fix user_registration form url

--- docs/js/user_registration.js
+++ docs/js/user_registration.js
@@ -6,7 +6,7 @@ registerButton.addEventListener('click', () => {
         password: document.querySelector('#registration_form_password').value,
         invitation_token: document.querySelector('#registration_form_invitation_token').value,
     }
-    fetch('http://127.0.0.1:8000/users', {
+    fetch('http://repo.cpmbits.com:8000/users', {
         method: 'POST',
         mode: 'cors',
         headers: {
